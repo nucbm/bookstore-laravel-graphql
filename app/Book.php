50ced9a5dc75950b37570c3913a4d9138897e78d
nucbm <stud.eeng@gmail.com>
2020-11-20 11:13:20 +0200

    First commit

---
+++ app/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //use HasFactory;
+    /*
+}
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{*/
+    protected $fillable = [
+        'title', 'author', 'language', 'year_published', 'isbn'
+    ];
+}
